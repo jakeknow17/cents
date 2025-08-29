ebba6044cb23225a6dc979f306aba44df46405a5
Jacob Knowlton <knowlton.jacob17@gmail.com>
2025-08-29 14:34:21 -0600

    Add navbar and prettier

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>,
-  )
+  );
 }
